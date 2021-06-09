0b958f645e9350caccb299edd3fc670448b27ca5
Thomas Sieverding <Thomas@bajix.com>
2021-06-08 19:18:21 -0700

    Add tests for SimpleLoader

--- channel-loader/src/simple_loader.rs
+++ channel-loader/src/simple_loader.rs
@@ -49,3 +49,45 @@ where
     }
   }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use crate::{attach_loader, define_static_loader};
+  use std::{collections::HashMap, iter};
+
+  #[derive(Default)]
+  pub struct FooLoader {}
+
+  #[derive(Clone, Debug, PartialEq, Eq)]
+  pub struct Foo;
+
+  #[async_trait::async_trait]
+  impl SimpleWorker for FooLoader {
+    type Key = ();
+    type Value = Foo;
+    type Error = ();
+
+    async fn load(keys: Vec<Self::Key>) -> Result<HashMap<Self::Key, Self::Value>, Self::Error> {
+      let mut data: HashMap<(), Foo> = HashMap::new();
+
+      data.extend(keys.into_iter().zip(iter::repeat(Foo)));
+
+      Ok(data)
+    }
+  }
+
+  define_static_loader!(SimpleLoader<FooLoader>);
+  attach_loader!(Foo, SimpleLoader<FooLoader>);
+
+  #[tokio::test]
+  async fn it_loads() -> Result<(), ()> {
+    booter::boot();
+
+    let data = Foo::load_by(()).await.unwrap()?;
+
+    assert!(data.is_some());
+
+    Ok(())
+  }
+}
