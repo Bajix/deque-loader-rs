a265332491d5d748f1db0838bb241788931344b7
Thomas Sieverding <Thomas@bajix.com>
2021-08-14 20:06:25 -0700

    Update Redis env-based connection management

--- deque-loader/src/redis/connection.rs
+++ deque-loader/src/redis/connection.rs
@@ -4,20 +4,29 @@ use std::env;
 use tokio::sync::watch;
 
 fn create_client() -> RedisResult<Client> {
-  let database_url = env::var("REDIS_URL").unwrap_or_else(|_| {
-    let hostname_env =
-      env::var("REDIS_SERVICE_HOSTNAME_ENV").unwrap_or_else(|_| String::from("REDIS_HOSTNAME"));
+  let redis_url_env = env::var("REDIS_URL_ENV").unwrap_or_else(|_| String::from("REDIS_URL"));
 
-    let port_env =
-      env::var("REDIS_SERVICE_PORT_ENV").unwrap_or_else(|_| String::from("REDIS_PORT"));
+  let database_url = env::var(redis_url_env).unwrap_or_else(|_| {
+    let hostname = {
+      let hostname_env =
+        env::var("REDIS_HOSTNAME_ENV").unwrap_or_else(|_| String::from("REDIS_HOSTNAME"));
 
-    let hostname = env::var(hostname_env).unwrap_or_else(|_| String::from("127.0.0.1"));
+      env::var(hostname_env).unwrap_or_else(|_| String::from("127.0.0.1"))
+    };
 
-    let port = env::var(port_env).unwrap_or_else(|_| String::from("6379"));
+    let port = {
+      let port_env = env::var("REDIS_PORT_ENV").unwrap_or_else(|_| String::from("REDIS_PORT"));
 
-    match env::var("REDIS_PASSWORD") {
-      Ok(password) => format!("redis://default:{}@{}:{}", password, hostname, port),
-      _ => format!("redis://{}:{}", hostname, port),
+      env::var(port_env).unwrap_or_else(|_| String::from("6379"))
+    };
+
+    let password_env =
+      env::var("REDIS_PASSWORD_ENV").unwrap_or_else(|_| String::from("REDIS_PASSWORD"));
+
+    if let Ok(password) = env::var(password_env) {
+      format!("redis://default:{}@{}:{}", password, hostname, port)
+    } else {
+      format!("redis://{}:{}", hostname, port)
     }
   });
 
